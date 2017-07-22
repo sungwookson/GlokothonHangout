5a51e41c6e7ab0d8f36328d80abffb7b451eaec4
Terry Yun <nooheat1228@gmail.com>
2017-07-22 15:35:42 +0900

    node init

---
+++ server/app.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let session = require('express-session');
+var bodyParser = require('body-parser');
+let morgan = require('morgan');
+
+
+let app = express();
+
+app.set('port', 8080);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(session({
+    key: 'entrykey',
+    secret: 'secret',
+    resave: false
+}));
+
+app.use(bodyParser.json());
+
+app.listen(app.get('port'), function () {
+    console.log("Started :: " + app.get('port'));
+});
