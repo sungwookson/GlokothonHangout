67562ecb58fdb68eef243beb591834b3305e309f
zhsir123 <zhsir123@gmail.com>
2017-07-22 14:54:51 +0900

    android init

---
+++ GlokothonTravelBuddy/app/src/main/java/com/example/dsm2016/glokothontravelbuddy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2016.glokothontravelbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
