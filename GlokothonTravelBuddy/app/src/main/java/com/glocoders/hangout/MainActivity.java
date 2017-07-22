65983405a2f2dbedc99af39e4b90440e1badaeda
Lee SangA <04_miffy@naver.com>
2017-07-22 23:45:52 +0900

    [sang-a] MainActivity 에서 PW는 *로 표시

--- GlokothonTravelBuddy/app/src/main/java/com/glocoders/hangout/MainActivity.java
+++ GlokothonTravelBuddy/app/src/main/java/com/glocoders/hangout/MainActivity.java
@@ -5,12 +5,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.glocoders.hangout.database.*;
+import com.glocoders.hangout.database.FirebaseHelper;
 import com.google.firebase.FirebaseApp;
+
+import static android.R.id.input;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseHelper fbHelper;
@@ -21,15 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // DB
         FirebaseApp.initializeApp(this);
         fbHelper = new FirebaseHelper();
         fbHelper.initUserAuth();
         fbHelper.setAuthListener();
 
+        /* Sign in, Sign up */
         Button sign_in = (Button) findViewById(R.id.sign_in);
         Button sign_up = (Button) findViewById(R.id.sign_up);
+
+        /* ID, PW */
         edit_id = (EditText)findViewById(R.id.sign_in_id);
         edit_pwd = (EditText)findViewById(R.id.sign_in_pw);
+
+        //PW
+        edit_pwd.setInputType( InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD );
+        edit_pwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
 
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
                     alertDialogBuilder.setTitle("로그인 실패");
+
                     alertDialogBuilder
                             .setMessage("이메일 혹은 비밀번호를 확인해주세요.")
                             .setCancelable(false)
@@ -56,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
                 startActivity(intent);
             }
