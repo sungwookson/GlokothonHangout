02ab1f8adf67067ff6e77ec3ab1e517ff681f7d2
Lee SangA <04_miffy@naver.com>
2017-07-22 15:26:00 +0900

    [sang-a] init2

---
+++ GlokothonTravelBuddy/app/src/main/java/travelbuddy/glokothontravelbuddy/OtherPeopleActivity.java
@@ -0,0 +1,13 @@
+package travelbuddy.glokothontravelbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherPeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_people);
+    }
+}
---
+++ GlokothonTravelBuddy/app/src/main/java/travelbuddy/glokothontravelbuddy/PeopleListActivity.java
@@ -0,0 +1,13 @@
+package travelbuddy.glokothontravelbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeopleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_list);
+    }
+}
